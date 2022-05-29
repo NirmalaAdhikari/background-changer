5a095d41728c8d930d2ec5af5b27da05fefe8e79
NirmalaAdhikari <106471155+NirmalaAdhikari@users.noreply.github.com>
2022-05-29 13:14:10 +0200

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,5 @@
+let red = document.getElementById("red");
+let aqua = document.getElementById("aqua");
+let blue = document.getElementById("blue");
+let yellow = document.getElementById("yellow");
+let btn = document.getElementsByClassName(".btn");
